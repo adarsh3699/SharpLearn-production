89b0bf37131d4388d727811c98e5a7859886b155
adarsh3699 <adarsh3699@gmail.com>
2024-03-22 22:12:34 +0530

    Add login and signup forms to LoginPage.js and update responsive styles

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -43,6 +43,98 @@ function LoginPage() {
 			<Toolbar />
 			{!isLoading ? (
 				<div className="loginPage">
+					<div
+						className="loginWapper mobileForm"
+						style={isSignupWapper ? { transform: 'rotateY(180deg)' } : null}
+					>
+						<form className="loginForm" onSubmit={handleUserLogin}>
+							<div className="loginWapperTitle">Login Here</div>
+							<input
+								type="email"
+								name="email"
+								placeholder="Email"
+								disabled={isApiLoading}
+								required
+								className="autoInput"
+								onKeyDown={handleMsgHideOnKeyUp}
+							/>
+							<input
+								type={ispasswordVisible ? 'text' : 'password'}
+								name="password"
+								placeholder="Password"
+								disabled={isApiLoading}
+								required
+								className="autoInput"
+								onKeyDown={handleMsgHideOnKeyUp}
+							/>
+							<div className="showPassContainer">
+								<input id="showPass" type="checkbox" onClick={handlePasswordVisibility} />
+								<label htmlFor="showPass">Show password</label>
+							</div>
+							<div className="authMSG">{msg}</div>
+							<button
+								className={isApiLoading ? 'loginBtnLoading loginBtn' : 'loginBtn'}
+								disabled={isApiLoading}
+							>
+								Login
+							</button>
+
+							<div className="signupdesc">Don't have an account</div>
+							<div className="createAccbtn" onClick={() => setIsSignupWapper(true)}>
+								Sign up here
+							</div>
+						</form>
+						<form className="signupForm" onSubmit={handleUserSignUpForm}>
+							<div className="loginWapperTitle">Sign Up Here</div>
+							<input
+								type="text"
+								name="userName"
+								placeholder="Full Name"
+								disabled={isApiLoading}
+								required
+								className="autoInput"
+								onKeyDown={handleMsgHideOnKeyUp}
+							/>
+							<input
+								type="email"
+								name="email"
+								placeholder="Email"
+								disabled={isApiLoading}
+								required
+								className="autoInput"
+								onKeyDown={handleMsgHideOnKeyUp}
+							/>
+							<input
+								type={ispasswordVisible ? 'text' : 'password'}
+								name="password"
+								placeholder="Password (8 digit)"
+								disabled={isApiLoading}
+								required
+								className="autoInput"
+								onKeyDown={handleMsgHideOnKeyUp}
+							/>
+							<input
+								type={ispasswordVisible ? 'text' : 'password'}
+								name="confPassword"
+								placeholder="Confirm Password (8 digit)"
+								disabled={isApiLoading}
+								required
+								className="autoInput"
+								onKeyDown={handleMsgHideOnKeyUp}
+							/>
+							<div className="authMSG">{msg}</div>
+							<button
+								className={isApiLoading ? 'loginBtnLoading loginBtn' : 'loginBtn'}
+								disabled={isApiLoading}
+							>
+								Sign Up
+							</button>
+							<div className="signupdesc">Already have an Account</div>
+							<div className="createAccbtn" onClick={() => setIsSignupWapper(false)}>
+								Login here
+							</div>
+						</form>
+					</div>
 					<div className="loginPageIntro">
 						<div className="text1">WelCome To</div>
 						<div className="text2">Employee SharpLearn</div>
