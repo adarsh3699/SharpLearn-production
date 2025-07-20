1cef516b5d7547b1f66ee58e002f7cb143fb7051
adarsh3699 <adarsh3699@gmail.com>
2025-07-20 14:20:40 +0530

    Update Vercel configuration by removing unnecessary fields and enhance CORS origins in config.js to include new domain.

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,12 @@
 const JWT_SECRET = 'sharplearn_jwt_secret_key_2024';
 const PORT = 4000;
 
-const CORS_ORIGINS = ['http://localhost:3000', 'http://localhost:3001', 'https://www.sharplearn.tech'];
+const CORS_ORIGINS = [
+	'http://localhost:3000',
+	'http://localhost:3001',
+	'https://sharplearn.bhemu.me',
+	'https://www.sharplearn.tech',
+];
 
 module.exports = {
 	JWT_SECRET,
